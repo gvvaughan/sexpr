9da3a86ae5a57b466f1e833cf7529670d9c19621
Gary V. Vaughan <gary@gnu.org>
2014-01-24 21:57:07 +1300

    specl: update specs to YAML format.
    * specs/atom_spec.lua, specs/parser_spec.lua: Move from here...
    * specs/atom_spec.yaml, specs/parser_spec.yaml: ...to here.
    Convert to YAML format.
    * specs/spec_helper.lua: Factor out symbol imports.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

---
+++ specs/spec_helper.lua
@@ -0,0 +1,5 @@
+sexpr = require "sexpr.lisp"
+
+Cons, Function, Nil, Number, String, Symbol, T =
+  sexpr.Cons, sexpr.Function, sexpr.Nil, sexpr.Number, sexpr.String, sexpr.Symbol, sexpr.T
+parse = sexpr.parse
